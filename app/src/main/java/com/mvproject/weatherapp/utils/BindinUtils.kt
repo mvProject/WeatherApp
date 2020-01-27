a175e5ec4ecb7d55438c52e1fa253e4c0f29d020
mvproject <awsom7@gmail.com>
2020-01-27 08:44:29 +0200

    image load fix

--- app/src/main/java/com/mvproject/weatherapp/utils/BindinUtils.kt
+++ app/src/main/java/com/mvproject/weatherapp/utils/BindinUtils.kt
@@ -4,8 +4,14 @@ import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
-
+/*
 @BindingAdapter("app:url" , "app:errorImage")
 fun loadImage(view: ImageView, url: String,  errorImage : Drawable) {
-    Picasso.get().load(url).error(errorImage).into(view)
+         Picasso.get().load(url).error(errorImage).into(view)
+}
+*/
+@BindingAdapter(value = ["app:url", "app:placeholder"], requireAll = false)
+fun loadImage(view: ImageView,url: String?,placeHolder: Drawable
+) {
+    Picasso.get().load(url).placeholder(placeHolder).into(view)
 }
